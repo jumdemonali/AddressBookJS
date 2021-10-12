f838be1f88b9c47fe24a68c0237443455da0ded7
jumdemonali <jumdemonali@gmail.com>
2021-10-12 19:16:06 +0530

    Added: View person by city or state in address book

--- addressBokk.js
+++ addressBokk.js
@@ -163,7 +163,23 @@ contactArray.forEach((contact) => CheckForDuplicates(contact));
             }
         }
         contactArray.filter(ContactByState);
-        console.log("\ncontacts by state is : " + numberOfState);}
+        console.log("\ncontacts by state is : " + numberOfState);
+        console.log("\nView person by city");
+
+        function CityMap(contact) {
+            return contact.city + " -> " + contact.firstName + "  " + contact.lastName;
+        }
+        let addressCityMap = contactArray.map(CityMap);
+        console.log(addressCityMap);
+    
+        //by state
+        console.log("\nview person by state");
+    
+        function StateMap(contactDetails) {
+            return contactDetails.state + " -> " + contactDetails.firstName + "  " + contactDetails.lastName;
+        }
+        let addressStateMap = contactArray.map(StateMap);
+        console.log(addressStateMap);}
 catch (e) {
     console.log(e);
 }
